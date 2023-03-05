a6fc799441a699f34835abfb026b03cd36317c59
Devin-Earl <RDevinEarl@gmail.com>
2023-03-04 20:35:35 -0600

    Update azureDownload.js and azureUpload.js to use double-quotes

--- azureDownload.js
+++ azureDownload.js
@@ -1,13 +1,13 @@
-require('dotenv').config();
-const { BlobServiceClient } = require('@azure/storage-blob');
-const fs = require('fs');
-const path = require('path');
+require("dotenv").config();
+const { BlobServiceClient } = require("@azure/storage-blob");
+const fs = require("fs");
+const path = require("path");
 
 const connectStr = process.env.AZURE_STORAGE_CONNECTION_STRING;
 
 const blobServiceClient = BlobServiceClient.fromConnectionString(connectStr);
 
-const outputDir = './output';
+const outputDir = "./output";
 if (!fs.existsSync(outputDir)) {
   fs.mkdirSync(outputDir);
 }
@@ -20,7 +20,9 @@ const downloadBlob = async (container, blobName, outputFileName) => {
     const response = await blobClient.download();
 
     const outputFilePath = path.join(outputDir, outputFileName);
-    await response.readableStreamBody.pipe(fs.createWriteStream(outputFilePath));
+    await response.readableStreamBody.pipe(
+      fs.createWriteStream(outputFilePath)
+    );
     console.log(`File downloaded to ${outputFilePath}`);
   } catch (err) {
     console.error(`Error downloading file ${blobName}: ${err.message}`);
@@ -29,7 +31,7 @@ const downloadBlob = async (container, blobName, outputFileName) => {
 
 const downloadFolder = async (container, prefix) => {
   const containerClient = blobServiceClient.getContainerClient(container);
-  const blobs = containerClient.listBlobsByHierarchy('/', { prefix: prefix });
+  const blobs = containerClient.listBlobsByHierarchy("/", { prefix: prefix });
 
   for await (const blob of blobs) {
     if (blob.kind === "blob") {
@@ -57,17 +59,22 @@ const downloadContainer = async (container) => {
 
 const azureDownload = async (program) => {
   try {
-    const container = program.container || (await ask('Please enter the container name: '));
-    const type = program.file ? 'file' : program.folder ? 'folder' : 'container';
+    const container =
+      program.container || (await ask("Please enter the container name: "));
+    const type = program.file
+      ? "file"
+      : program.folder
+      ? "folder"
+      : "container";
 
     switch (type) {
-      case 'file': {
+      case "file": {
         const blobName = program.file;
         const outputFileName = path.basename(blobName);
         await downloadBlob(container, blobName, outputFileName);
         break;
       }
-      case 'folder': {
+      case "folder": {
         const prefix = program.folder;
         await downloadFolder(container, prefix);
         break;
@@ -83,7 +90,7 @@ const azureDownload = async (program) => {
 
 const ask = (question) => {
   return new Promise((resolve, reject) => {
-    const readline = require('readline').createInterface({
+    const readline = require("readline").createInterface({
       input: process.stdin,
       output: process.stdout,
     });
